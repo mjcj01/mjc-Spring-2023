9dfe1d1cbb1ec8b9a892093d66bdaf58722f4a65
Michael Cattell <mjclego@icloud.com>
2023-02-04 10:59:22 -0500

    Added check for middle and high schools

--- UROC/bcps_nces_codes/shp_merge_check.R
+++ UROC/bcps_nces_codes/shp_merge_check.R
@@ -18,12 +18,38 @@ hs_2010_shp <- st_read("UROC/school_boundary_files/HS2010/Baltimore_County_2010_
 es_2010 <- merge(es_2010_nces, es_2010_shp, by.x = "school_name", by.y = "schnam", all = TRUE) %>%
   st_as_sf() %>%
   mutate("on_nces_key" = ifelse(is.na(nces_school_id), "FALSE", "TRUE"),
-         "has_geometry" = ifelse(is.na(st_dimension(es_2010)), "FALSE", "TRUE"),
+         "has_geometry" = ifelse(is.na(st_dimension(geometry)), "FALSE", "TRUE"),
          "nces_ids_same" = ifelse(is.na(nces_school_id), "No NCES Key ID",
                                   ifelse(is.na(NCES_ID), "No SHP ID",
                                   ifelse(NCES_ID == nces_school_id, "TRUE", "FALSE"))))
+
+ms_2010 <- merge(ms_2010_nces, ms_2010_shp, by.x = "school_name", by.y = "schnam", all = TRUE) %>%
+  st_as_sf() %>%
+  mutate("on_nces_key" = ifelse(is.na(nces_school_id), "FALSE", "TRUE"),
+         "has_geometry" = ifelse(is.na(st_dimension(geometry)), "FALSE", "TRUE"),
+         "nces_ids_same" = ifelse(is.na(nces_school_id), "No NCES Key ID",
+                                  ifelse(is.na(NCES_ID), "No SHP ID",
+                                         ifelse(NCES_ID == nces_school_id, "TRUE", "FALSE"))))
+
+hs_2010 <- merge(hs_2010_nces, hs_2010_shp, by.x = "school_name", by.y = "schnam", all = TRUE) %>%
+  st_as_sf() %>%
+  mutate("on_nces_key" = ifelse(is.na(nces_school_id), "FALSE", "TRUE"),
+         "has_geometry" = ifelse(is.na(st_dimension(geometry)), "FALSE", "TRUE"),
+         "nces_ids_same" = ifelse(is.na(nces_school_id), "No NCES Key ID",
+                                  ifelse(is.na(NCES_ID), "No SHP ID",
+                                         ifelse(NCES_ID == nces_school_id, "TRUE", "FALSE"))))
 
 es_2010 %>%
   as.data.frame() %>%
   select(school_name, on_nces_key, has_geometry, nces_ids_same) %>%
   write.csv(., "UROC/bcps_nces_codes/checks/es_2010_check.csv", row.names = FALSE)
+
+ms_2010 %>%
+  as.data.frame() %>%
+  select(school_name, on_nces_key, has_geometry, nces_ids_same) %>%
+  write.csv(., "UROC/bcps_nces_codes/checks/ms_2010_check.csv", row.names = FALSE)
+
+hs_2010 %>%
+  as.data.frame() %>%
+  select(school_name, on_nces_key, has_geometry, nces_ids_same) %>%
+  write.csv(., "UROC/bcps_nces_codes/checks/hs_2010_check.csv", row.names = FALSE)
